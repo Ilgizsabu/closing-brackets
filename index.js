6ce4b53f8e079f3e8c2ed6bf8ac80dca4bc45b66
Ilgiz Saburov <barysaburov@gmail.com>
2024-09-09 11:04:48 +0300

    fix to bags

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-let stack = [];
+
+const textUser = prompt('Enter text');
+const check = Array.from(textUser);
+debugger;
+const checkIfBalance = (data) => {
+    const stack = [];
+    for (const symbol of data) {
+        if (symbol === '(' || symbol === '{' || symbol === '[') {
+            stack.push(symbol);
+        } else if (symbol === ')' || symbol === '}' || symbol === ']') {
+            const last = stack.pop();
+            if (
+                (symbol === ')' && last !== '(') || 
+                (symbol === '}' && last !== '{') || 
+                (symbol === ']' && last !== '[')
+            ) {
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
+};
+
+console.log(checkIfBalance(check));
